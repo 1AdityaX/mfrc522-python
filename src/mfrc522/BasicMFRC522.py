5d045651c75f1a1a060570cd9ed2a5307b9e8bfd
ChaosXsniffer <biscuitbot07@gmail.com>
2023-04-24 13:44:51 +0530

    formated

--- src/mfrc522/BasicMFRC522.py
+++ src/mfrc522/BasicMFRC522.py
@@ -1,16 +1,19 @@
 from . import MFRC522
 
+
 class BasicMFRC522:
     def __init__(self, KEY=[0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]):
         self.MFRC522 = MFRC522()
         self.KEY = KEY
-    
+
     def read_sector(self, trailer_block=11):
-        id, text = self.read_no_block(trailer_block, (trailer_block-3, trailer_block-2, trailer_block-1))
+        id, text = self.read_no_block(
+            trailer_block, (trailer_block-3, trailer_block-2, trailer_block-1))
         while not id:
-            id, text = self.read_no_block(trailer_block, (trailer_block-3, trailer_block-2, trailer_block-1))
+            id, text = self.read_no_block(
+                trailer_block, (trailer_block-3, trailer_block-2, trailer_block-1))
         return id, text
-    
+
     def read_sectors(self, trailer_blocks=[11]):
         text_all = ''
         for trailer_block in trailer_blocks:
@@ -72,7 +75,8 @@ class BasicMFRC522:
         text_all = ''
         for i in range(0, len(trailer_blocks)):
             try:
-                id, text = self.write_sector(text_formated_list[i], trailer_blocks[i])
+                id, text = self.write_sector(
+                    text_formated_list[i], trailer_blocks[i])
                 text_all += text
             except IndexError:
                 pass
@@ -87,7 +91,8 @@ class BasicMFRC522:
             return None, None
         id = self.uid_to_num(uid)
         self.MFRC522.SelectTag(uid)
-        status = self.MFRC522.Authenticate(self.MFRC522.PICC_AUTHENT1A, trailer_block, self.KEY, uid)
+        status = self.MFRC522.Authenticate(
+            self.MFRC522.PICC_AUTHENT1A, trailer_block, self.KEY, uid)
         self.MFRC522.ReadTag(trailer_block)
         try:
             if status == self.MFRC522.MI_OK:
@@ -104,20 +109,6 @@ class BasicMFRC522:
             self.MFRC522.StopCrypto1()
             return None, None
 
-    def uid_to_num(self, uid):
-        n = 0
-        for i in range(0, 5):
-            n = n * 256 + uid[i]
-        return n
-
-    def split_string(self, s):
-        l = list()
-        for i in range(0, len(s), 48):
-            l.append(s[i:i+48]) 
-        if len(l[-1]) < 48:
-            l[-1] += '\0'*(48-len(l[-1]))
-        return l
-    
     def clear_no_sector(self, trailer_block):
         (status, TagType) = self.MFRC522.Request(self.MFRC522.PICC_REQIDL)
         if status != self.MFRC522.MI_OK:
@@ -127,7 +118,8 @@ class BasicMFRC522:
             return None
         id = self.uid_to_num(uid)
         self.MFRC522.SelectTag(uid)
-        status = self.MFRC522.Authenticate(self.MFRC522.PICC_AUTHENT1A, trailer_block, self.KEY, uid)
+        status = self.MFRC522.Authenticate(
+            self.MFRC522.PICC_AUTHENT1A, trailer_block, self.KEY, uid)
         self.MFRC522.ReadTag(trailer_block)
         block_addr = [trailer_block-3, trailer_block-2, trailer_block-1]
         try:
@@ -146,3 +138,17 @@ class BasicMFRC522:
         while not id:
             id = self.clear_no_sector(trailer_block)
         return id
+
+    def uid_to_num(self, uid):
+        n = 0
+        for i in range(0, 5):
+            n = n * 256 + uid[i]
+        return n
+
+    def split_string(self, s):
+        l = list()
+        for i in range(0, len(s), 48):
+            l.append(s[i:i+48])
+        if len(l[-1]) < 48:
+            l[-1] += '\0'*(48-len(l[-1]))
+        return l
